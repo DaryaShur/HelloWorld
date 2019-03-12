1579f9f6c475dd1e41c8c69e257b1671bdfd0302
Darya <47759695+DaryaShur@users.noreply.github.com>
2019-03-12 07:27:16 -0400

    Add files via upload

---
+++ HelloWorld/src/java/com/darya19/bean/UserBean.java
@@ -0,0 +1,29 @@
+package com.darya19.bean;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+
+@Named(value = "userBean")
+@SessionScoped
+public class UserBean implements Serializable {
+
+    
+    public UserBean() {
+    }
+    
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String display(){
+        return "display";
+    }
+}
